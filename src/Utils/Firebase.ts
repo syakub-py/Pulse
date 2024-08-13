71f11e40e7adb7b5429bf90702cd4d006866d0b3
Sam <syakubov101@gmail.com>
2024-08-13 11:43:16 -0400

    fix env errors

--- src/Utils/Firebase.ts
+++ src/Utils/Firebase.ts
@@ -3,25 +3,18 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/analytics";
 import "firebase/compat/storage";
-import {
-	API_KEY,
-	AUTH_DOMAIN,
-	PROJECT_ID,
-	STORAGE_BUCKET,
-	MESSAGING_SENDER_ID,
-	APP_ID,
-	MEASUREMENT_ID
-} from "@env";
+import config from "../../env";
 
 const firebaseConfig = {
-	apiKey: API_KEY,
-	authDomain: AUTH_DOMAIN,
-	projectId: PROJECT_ID,
-	storageBucket: STORAGE_BUCKET,
-	messagingSenderId: MESSAGING_SENDER_ID,
-	appId: APP_ID,
-	measurementId: MEASUREMENT_ID,
+	apiKey: config.API_KEY,
+	authDomain: config.AUTH_DOMAIN,
+	projectId: config.PROJECT_ID,
+	storageBucket: config.STORAGE_BUCKET,
+	messagingSenderId: config.MESSAGING_SENDER_ID,
+	appId: config.APP_ID,
+	measurementId: config.MEASUREMENT_ID,
 };
+
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
