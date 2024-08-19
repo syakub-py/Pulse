d6f8f304ad98f28e61a67e52c0d3669c1596d4a8
atamayev <75034340+atamayev@users.noreply.github.com>
2024-08-18 22:41:23 -0400

    Try-catch

--- src/Hooks/useGetLeasesAndTenants.ts
+++ src/Hooks/useGetLeasesAndTenants.ts
@@ -10,33 +10,36 @@ export default function useGetLeasesAndTenants() {
 	const authContext = useAuthContext();
 
 	const fetchLeasesAndTenants = useCallback(async () => {
-		if (
-			_.isEmpty(authContext.uid) ||
-			_.isUndefined(appContext.SelectedProperty?.PropertyId) ||
-			!appContext.SelectedProperty.isRental
-		) return;
-
-		const response = await LeaseService.getLeases(appContext.SelectedProperty.PropertyId);
-		if (appContext.isHTTPError(response)) {
-			alert(response.message);
+		try {
+			if (
+				_.isEmpty(authContext.uid) ||
+				_.isUndefined(appContext.SelectedProperty?.PropertyId) ||
+				!appContext.SelectedProperty.isRental
+			) return;
+	
+			const response = await LeaseService.getLeases(appContext.SelectedProperty.PropertyId);
+			if (appContext.isHTTPError(response)) {
+				alert(response.message);
+				throw Error(response.message);
+			}
+	
+			if (_.isEmpty(appContext.Tenants)){
+				const tenants= await TenantService.getTenants(authContext.uid);
+				appContext.setTenants(tenants);
+			}
+	
+			if (!_.isNil(response)){
+				const leases = JSON.parse(response.toString()) as Lease[];
+				appContext.setPropertyLeases(leases.map(lease => {
+					const matchingTenant = appContext.Tenants.find(tenant => tenant.LeaseId === lease.LeaseId);
+					return !_.isUndefined(matchingTenant) ? { ...lease, TenantName: matchingTenant.Name } : lease;
+				}));
+			}
+		} catch (error) {
+			console.error(error);
+		} finally {
 			authContext.isLoading = false;
-			return [];
 		}
-
-		if (_.isEmpty(appContext.Tenants)){
-			const tenants= await TenantService.getTenants(authContext.uid);
-			appContext.setTenants(tenants);
-		}
-
-		if (!_.isNil(response)){
-			const leases = JSON.parse(response.toString()) as Lease[];
-			appContext.setPropertyLeases(leases.map(lease => {
-				const matchingTenant = appContext.Tenants.find(tenant => tenant.LeaseId === lease.LeaseId);
-				return !_.isUndefined(matchingTenant) ? { ...lease, TenantName: matchingTenant.Name } : lease;
-			}));
-		}
-
-		authContext.isLoading = false;
 		/* eslint-disable react-hooks/exhaustive-deps */
 	}, [authContext.uid, appContext.SelectedProperty]);
 
