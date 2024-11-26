44525dcc0968147922574f65d1e40fa1019ca33f
syakub-py <75036399+syakub-py@users.noreply.github.com>
2024-11-26 13:13:37 -0500

    Update env.ts

--- env.ts
+++ env.ts
@@ -11,13 +11,13 @@ interface Config {
 }
 
 const config: Config = {
-	API_KEY: "AIzaSyAS0OLIeH01QAyHZQxILWWUu3I2PJm3xz4",
-	AUTH_DOMAIN: "nyle-bd594.firebaseapp.com",
-	PROJECT_ID: "nyle-bd594",
-	STORAGE_BUCKET: "nyle-bd594.appspot.com",
-	MESSAGING_SENDER_ID: "616674242131",
-	APP_ID: "1:616674242131:web:7788508192e82a84b660a6",
-	MEASUREMENT_ID: "G-70E5N2DBMC",
+	API_KEY: "",
+	AUTH_DOMAIN: "",
+	PROJECT_ID: "",
+	STORAGE_BUCKET: "",
+	MESSAGING_SENDER_ID: "",
+	APP_ID: "",
+	MEASUREMENT_ID: "",
 	DEFAULT_PROFILE_PICTURE: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
 	BASE_URL:"http://127.0.0.1:8000"
 };
