ff227c5002f09c39e0cc74413cf29d7d1d3965d4
syakub-py <75036399+syakub-py@users.noreply.github.com>
2024-11-26 13:14:17 -0500

    Update env.ts

--- env.ts
+++ env.ts
@@ -11,13 +11,13 @@ interface Config {
 }
 
 const config: Config = {
-	API_KEY: "AIzaSyDbtKzqRUe-8lJL9Ki7CCrTOmmGcnX8ks8",
-	AUTH_DOMAIN: "pulse-91f49.firebaseapp.com",
-	PROJECT_ID: "pulse-91f49",
-	STORAGE_BUCKET: "pulse-91f49.firebasestorage.app",
-	MESSAGING_SENDER_ID: "877426915878",
-	APP_ID: "1:877426915878:web:e4646738cdcf6f6c2844e7",
-	MEASUREMENT_ID: "G-QB5DSRQNRZ",
+	API_KEY: "",
+	AUTH_DOMAIN: "",
+	PROJECT_ID: "",
+	STORAGE_BUCKET: "",
+	MESSAGING_SENDER_ID: "",
+	APP_ID: "",
+	MEASUREMENT_ID: "",
 	DEFAULT_PROFILE_PICTURE: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
 	BASE_URL:"http://127.0.0.1:8000"
 };
